503b52c643a303ab7b3e1aef19c32e5d9e244169
Ivan <ivanchernetskey@mail.ru>
2021-02-06 16:28:03 +0300

    modify method destroy() for destroy EventListener

--- plugins/model.js
+++ plugins/model.js
@@ -28,9 +28,13 @@ $.modal = function(options) {
     const ANIMATION_SPEED = 200
     const $modal = _createModal(options)
     let closing = false
+    let destroyed = false
 
     const modal = {
         open() {
+            if(destroyed) {
+                console.log('Modal is destroyed')
+            }
             !closing && $modal.classList.add('open')
         },
         close() {
@@ -41,16 +45,22 @@ $.modal = function(options) {
                 $modal.classList.remove('hide')
                 closing = false
             }, ANIMATION_SPEED)
-        },
-        destroy() {}
+        }
     }
 
-    $modal.addEventListener('click', event => {
-        console.log('clicked', event.target.dataset.close)
+    const listener = event => {
         if(event.target.dataset.close) {
             modal.close()
         }
-    })
+    }
 
-    return modal
+    $modal.addEventListener('click', listener)
+
+    return Object.assign(modal, {
+        destroy() {
+            $modal.parentNode.removeChild($modal)
+            $modal.removeEventListener('click', listener)
+            destroyed = true
+        }
+    })
 }
