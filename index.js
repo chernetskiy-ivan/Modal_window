b804c474c501e85805b8df515dc424667ee76987
Ivan <ivanchernetskey@mail.ru>
2021-02-06 15:56:03 +0300

    cteated logic for optins

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 //$ всего лишь объект, условная библиотека
-const modal = $.modal()
+const modal = $.modal({
+    title: 'Ivan Modal',
+    closable: true,
+    content: `
+    <h4>Modal is working</h4>
+    <p>Lorem ipsum dolor sit.</p>
+`,
+    width: '400px'
+})
