81cead7db048af55e83693b83dbed6d3496bb33a
Ivan <ivanchernetskey@mail.ru>
2021-02-07 17:32:52 +0300

    create logic for show price in modal window on every products

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const toHTML = fruit => `
                 <img class="card-img-top" style="height: 300px;" src="${fruit.img}" alt="${fruit.title}">
                 <div class="card-body">
                     <h5 class="card-title">${fruit.title}</h5>
-                    <a href="#" class="btn btn-primary" data-btn="price">Посмотреть цену</a>
+                    <a href="#" class="btn btn-primary" data-btn="price" data-id="${fruit.id}">Посмотреть цену</a>
                     <a href="#" class="btn btn-danger">Удалить</a>
                 </div>
             </div>
@@ -23,23 +23,14 @@ function render() {
 
 render()
 
-const modal = $.modal({
-    title: 'Ivan Modal',
+const priceModal = $.modal({
+    title: 'Цена на Товар',
     closable: true,
-    content: `
-    <p>Lorem ipsum dolor sit.</p>
-    <p>Lorem ipsum dolor sit.</p>
-`,
     width: '400px',
     footerButtons:[
-        {text: 'Ok', type: 'primary', handler(){
-            console.log('Primary btn clicked')
-                modal.close()
-            }},
-        {text: 'Cancel', type: 'danger', handler(){
-            console.log('Danger btn clicked')
-                modal.close()
-            }}
+        {text: 'Закрыть', type: 'primary', handler(){
+            priceModal.close()
+        }}
     ]
 })
 
@@ -48,8 +39,11 @@ document.addEventListener('click', event => {
     event.preventDefault()
 
     const btnType = event.target.dataset.btn
+    const id = +event.target.dataset.id
 
     if(btnType === 'price') {
-        console.log('price')
+        const fruit = fruits.find(f => f.id === id)
+        priceModal.setContent(`<p>Цена на ${fruit.title}: <strong>${fruit.price}$</strong></p>`)
+        priceModal.open()
     }
 })
